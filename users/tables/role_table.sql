2418f32f1d81fb71b7d69c8eccfa41a4f2d22ddf
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-07-29 15:14:50 -0600

    Make name unique.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- users/tables/role_table.sql
+++ users/tables/role_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "users"."role"
 (
   "id"   SMALLINT    NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "name" VARCHAR(64) NOT NULL CHECK ("name" <> '')
+  "name" VARCHAR(64) NOT NULL UNIQUE CHECK ("name" <> '')
 );
 
 COMMENT ON TABLE "users"."role"
