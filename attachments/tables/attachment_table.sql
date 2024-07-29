57ac5c7411ea5e756afc940675a69f695132cab5
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-07-29 15:15:13 -0600

    Update table definition.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- attachments/tables/attachment_table.sql
+++ attachments/tables/attachment_table.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS "attachments"."attachment"
 (
   "attachment_id" uuid          NOT NULL PRIMARY KEY DEFAULT "addons"."uuid_generate_v4"(),
   "owner_uuid"    uuid          NOT NULL REFERENCES "users"."user" ("user_uuid"),
-  "task_id"       uuid          NOT NULL REFERENCES "tasks"."task" ("task_id"),
+  "task_uuid"     uuid          NOT NULL REFERENCES "tasks"."task" ("task_uuid"),
   "file_name"     VARCHAR(255)  NOT NULL,
   "file_url"      VARCHAR(2048) NOT NULL,
-  "created_at"    timestamptz   NOT NULL             DEFAULT now(),
-  "updated_at"    timestamptz   NOT NULL             DEFAULT now()
+  "created_at"    timestamptz   NOT NULL             DEFAULT current_timestamp,
+  "updated_at"    timestamptz   NOT NULL             DEFAULT current_timestamp
 );
 
 COMMENT ON TABLE "attachments"."attachment"
