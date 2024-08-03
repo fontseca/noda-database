bc5b46936c7e729bc6e02abec77fe6830aa9c269
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:27:38 -0600

    Delete group when delete owner.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- groups/tables/group_table.sql
+++ groups/tables/group_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "groups"."group"
 (
   "group_uuid"  uuid        NOT NULL PRIMARY KEY DEFAULT "addons"."uuid_generate_v4"(),
-  "owner_uuid"  uuid        NOT NULL REFERENCES "users"."user" ("user_uuid"),
+  "owner_uuid"  uuid        NOT NULL REFERENCES "users"."user" ("user_uuid") ON DELETE CASCADE,
   "name"        VARCHAR(64) NOT NULL CHECK ("name" <> ''),
   "description" VARCHAR(512)                     DEFAULT NULL,
   "created_at"  timestamptz NOT NULL             DEFAULT current_timestamp,
