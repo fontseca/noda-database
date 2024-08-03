934308bf5573eb5c0361c8ef5100f719ed5498e8
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:51:58 -0600

    On delete owner, cascade; and make title not unique.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- tasks/tables/task_table.sql
+++ tasks/tables/task_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "tasks"."task"
 (
   "task_uuid"        uuid PRIMARY KEY          NOT NULL DEFAULT "addons"."uuid_generate_v4"(),
-  "owner_uuid"       uuid                      NOT NULL REFERENCES "users"."user" ("user_uuid"),
+  "owner_uuid"       uuid                      NOT NULL REFERENCES "users"."user" ("user_uuid") ON DELETE CASCADE,
   "list_uuid"        uuid                      NOT NULL REFERENCES "lists"."list" ("list_uuid") ON DELETE CASCADE,
   "position_in_list" "common"."pos_t"          NOT NULL,
-  "title"            VARCHAR(128) UNIQUE       NOT NULL,
+  "title"            VARCHAR(128)              NOT NULL,
   "headline"         VARCHAR(64)                        DEFAULT NULL,
   "description"      VARCHAR(512)                       DEFAULT NULL,
   "priority"         "tasks"."task_priority_t" NOT NULL,
