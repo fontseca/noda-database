b831f3a28585fb5871d351f957a9698a65236d0f
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:53:13 -0600

    On delete task, cascade; and order unique per list.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- tasks/tables/step_table.sql
+++ tasks/tables/step_table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS "tasks"."step"
 (
   "step_uuid"    uuid PRIMARY KEY NOT NULL DEFAULT "addons"."uuid_generate_v4"(),
-  "task_uuid"    uuid             NOT NULL REFERENCES "tasks"."task" ("task_uuid"),
-  "order"        "common"."pos_t" NOT NULL UNIQUE,
+  "task_uuid"    uuid             NOT NULL REFERENCES "tasks"."task" ("task_uuid") ON DELETE CASCADE,
+  "order"        "common"."pos_t" NOT NULL,
   "description"  VARCHAR(512)              DEFAULT NULL,
   "completed_at" timestamptz               DEFAULT NULL,
   "created_at"   timestamptz      NOT NULL DEFAULT current_timestamp,
-  "updated_at"   timestamptz      NOT NULL DEFAULT current_timestamp
+  "updated_at"   timestamptz      NOT NULL DEFAULT current_timestamp,
+  UNIQUE ("task_uuid", "order")
 );
 
 COMMENT ON TABLE "tasks"."step"
