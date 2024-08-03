506d7514f83f123b79d2bd1a70355e05def7a847
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:27:04 -0600

    Delete list when delete owner.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- lists/tables/list_table.sql
+++ lists/tables/list_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "lists"."list"
 (
   "list_uuid"   uuid        NOT NULL PRIMARY KEY DEFAULT "addons"."uuid_generate_v4"(),
-  "owner_uuid"  uuid        NOT NULL REFERENCES "users"."user" ("user_uuid"),
+  "owner_uuid"  uuid        NOT NULL REFERENCES "users"."user" ("user_uuid") ON DELETE CASCADE,
   "group_uuid"  uuid                             DEFAULT NULL REFERENCES "groups"."group" ("group_uuid") ON DELETE CASCADE,
   "name"        VARCHAR(64) NOT NULL CHECK ("name" <> ''),
   "description" VARCHAR(512)                     DEFAULT NULL,
