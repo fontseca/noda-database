92a364d7467738852323852f8c72fb81b417f4da
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:25:01 -0600

    Cascade on delete.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- lists/tables/user_special_list_table.sql
+++ lists/tables/user_special_list_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "lists"."user_special_list"
 (
   "user_special_list_uuid" uuid PRIMARY KEY              NOT NULL DEFAULT "addons"."uuid_generate_v4"(),
-  "user_uuid"              uuid REFERENCES "users"."user" ("user_uuid"),
-  "list_uuid"              uuid REFERENCES "lists"."list" ("list_uuid"),
+  "user_uuid"              uuid REFERENCES "users"."user" ("user_uuid") ON DELETE CASCADE,
+  "list_uuid"              uuid REFERENCES "lists"."list" ("list_uuid") ON DELETE CASCADE,
   "list_type"              "lists"."special_list_type_t" NOT NULL
 );
