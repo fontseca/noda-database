50bdb2eeb335f4233a1f405549ee94399be13be6
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-02 21:52:39 -0600

    On delete owner, cascade; and make name not unique.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- tasks/tables/tag_table.sql
+++ tasks/tables/tag_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "tasks"."tag"
 (
   "tag_uuid"    uuid                   NOT NULL PRIMARY KEY DEFAULT "addons"."uuid_generate_v4"(),
-  "owner_uuid"  uuid                   NOT NULL REFERENCES "users"."user" ("user_uuid"),
-  "name"        VARCHAR(50)            NOT NULL UNIQUE,
+  "owner_uuid"  uuid                   NOT NULL REFERENCES "users"."user" ("user_uuid") ON DELETE CASCADE,
+  "name"        VARCHAR(50)            NOT NULL,
   "description" VARCHAR(512)                                DEFAULT NULL,
   "color"       "common"."tag_color_t" NOT NULL             DEFAULT 'FFF',
   "created_at"  timestamptz            NOT NULL             DEFAULT current_timestamp,
