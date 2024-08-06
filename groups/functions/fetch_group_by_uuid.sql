5a03c188cad07e48a85a81b7804425afaa23cf97
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-05 18:39:45 -0600

    Remove order and re-format.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- groups/functions/fetch_group_by_uuid.sql
+++ groups/functions/fetch_group_by_uuid.sql
@@ -1,18 +1,19 @@
-CREATE OR REPLACE FUNCTION "groups"."fetch_by_uuid"(
-  IN "p_owner_id" "groups"."group"."owner_uuid"%TYPE,
-  IN "p_group_uuid" "groups"."group"."group_uuid"%TYPE
+CREATE OR REPLACE FUNCTION "groups"."fetch_by_uuid"
+(
+  IN p_owner_uuid "groups"."group"."owner_uuid"%TYPE,
+  IN p_group_uuid "groups"."group"."group_uuid"%TYPE
 )
   RETURNS SETOF "groups"."group"
   LANGUAGE 'plpgsql'
 AS
 $$
 BEGIN
-  CALL "users"."assert_exists"("p_owner_id");
-  CALL "groups"."assert_exists"("p_owner_id", "p_group_uuid");
+  CALL "users"."assert_exists"(p_owner_uuid);
+  CALL "groups"."assert_exists"("p_owner_uuid", p_group_uuid);
   RETURN QUERY
     SELECT *
-    FROM "groups"."group"
-    WHERE "group_uuid" = "p_group_uuid"
-      AND "owner_uuid" = "p_owner_id";
+      FROM "groups"."group"
+     WHERE "group_uuid" = p_group_uuid
+       AND "owner_uuid" = p_owner_uuid;
 END;
 $$;
